598c14bbfe89386f5fc432baaeff982512595d91
Cory Zue <czue@dimagi.com>
2009-04-14 12:21:24 +0100

    add an extremely ugly, but functional, display of recursive data for trees that have loops

--- views.py
+++ views.py
@@ -28,26 +28,48 @@ def data(req, id = None):
     allTrees = Tree.objects.all()
     # ok, the plan is to generate a table of responses per state, but this is tricky with loops.
     # the first thing we'll do is get all the possible states you can reach from the tree
-    # then we'll tabulate the results of each state as a total
+    # then we'll tabulate the results of each state's first value
     # then we'll look at paths, by using the concatenated list of states taken for that path as a key
-    # but first things first.  let's get all the states you can reach
     if len(allTrees) != 0:
         t = get_tree(id)
         all_states = get_all_states(t)
         loops = has_loops(t) 
         if not loops:
             # this is the easy case.  just create one column per state and then display the results
-            # print "==== all states === \n%s\n==== end all states ==== " % all_states
-            # now we're going to build a data structure to hold the submissions.  
-            # It's just going to be a list of lists, where each sub-list represents
-            # a single submission.  We'll also keep a second list for the column headers
             sessions = Session.objects.all().filter(tree=t)
             return render_to_response("tree/data.html",
-                                      { "trees": allTrees, "t": t, "states" : all_states, "sessions" : sessions },
+                                      { "trees": allTrees, "t": t, "states" : all_states, "sessions" : sessions, "loops" : loops},
                                       context_instance=RequestContext(req))
         else: 
-            # todo
-            pass
+            # here what we want is a table where the columns are every unique path through the 
+            # tree, and the rows are the sessions, with the paths filled in.
+            # Since there are loops we have to generate the list of unique paths from
+            # the session data.  
+            # So we get every session, and for every entry in that session we get the path.
+            # If we haven't seen the path we add it to the master list.  
+            # Then we walk back through the master list, and walk through each session to
+            # see if it has an entry matching that path, and if so set the answer.  
+            sessions = Session.objects.all().filter(tree=t)
+            paths = {}
+            # i think paths will be a dictionary of paths to dictionaries of sessions to answers
+            # e.g. { <path> : { <session> : <answer>}
+            # which will allow us to iterate through paths and say:
+            # if paths[path].has_key(session):
+            #    this_cell = paths[path][session]
+            for session in sessions:
+                entries = session.entry_set.all().order_by('sequence_id')
+                path = ''
+                for entry in entries:
+                    path = path + str(entry.transition.current_state.id) + '/'
+                    entry.path = path
+                    if paths.has_key(path):
+                        paths[path][session] = entry.transition.answer
+                    else:
+                        paths[path] = { session : entry.transition.answer }
+            print paths
+            return render_to_response("tree/data.html",
+                                      { "trees": allTrees, "t": t, "paths" : paths, "sessions" : sessions, "loops" : loops },
+                                      context_instance=RequestContext(req))
         print all_states
         # now we need to map all states to answers
         states_w_answers = {}
@@ -69,8 +91,6 @@ def data(req, id = None):
             context_instance=RequestContext(req))
 
 def export(req, id = None):
-    #print states
-    #print sessions
     t = get_tree(id)
     all_states = get_all_states(t)
     loops = has_loops(t) 
@@ -87,7 +107,6 @@ def export(req, id = None):
             states_w_transitions = {}
             for transition in transitions:
                 states_w_transitions[transition.current_state] = transition
-            #print states_w_transitions
             for state in all_states:
                 if states_w_transitions.has_key(state):
                     values.append(states_w_transitions[state].answer)
@@ -146,10 +165,16 @@ def path_has_loops(path):
       
     
 def get_tree(id):
+    '''Gets a tree.  If id is specified it gets the tree with that Id.
+       If Id is not specified it gets the latest tree.  If there are 
+       no trees, it returns an empty tree.'''
     if id:
         return Tree.objects.get(id=id)
     else:
-        return Tree.objects.all()[len(Tree.objects.all()) - 1]  
+        if len(Tree.objects.all()) > 0:
+            return Tree.objects.all()[len(Tree.objects.all()) - 1]
+        else:
+            return Tree()  
     
 def get_all_states(tree):
     all_states = []
